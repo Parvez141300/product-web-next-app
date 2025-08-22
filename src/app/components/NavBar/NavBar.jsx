9cf445a47abf88e223fc1816973c0ed9454d6cb1
Parvez141300 <parvezhossain744471@gmail.com>
2025-08-22 10:57:59 +0600

    modified the navbar

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -1,71 +1,85 @@
-
+"use client";
 import Link from "next/link";
 import React from "react";
 import Button from "../Button/Button";
 import ThemeToggle from "./shared/ThemeToggle";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const pathname = usePathname();
   const links = (
     <>
       <li>
         <Link href={"/"}>Home</Link>
       </li>
       <li>
-        <Link href={'/products'}>Products</Link>
+        <Link href={"/products"}>Products</Link>
       </li>
       <li>
         <a>Dashboard</a>
       </li>
     </>
   );
-  return (
-    <div className="navbar bg-base-100 w-11/12 mx-auto">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost px-0 lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+
+  if (!pathname.includes("dashboard")) {
+    return (
+      <div className="navbar bg-base-100 w-11/12 mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost px-0 lg:hidden"
             >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            {links}
-          </ul>
+          <Link href={"/"} className="btn btn-ghost text-xl px-0">
+            EasyPro
+          </Link>
         </div>
-        <Link href={'/'} className="btn btn-ghost text-xl px-0">EasyPro</Link>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end gap-2">
+          {/* theme toggle */}
+          <ThemeToggle></ThemeToggle>
+          {/* authentication links */}
+          <Link href={"/login"}>
+            <Button className={"btn-primary btn-outline rounded-lg"}>
+              Login
+            </Button>
+          </Link>
+          <Link href={"/register"}>
+            <Button className={"btn-primary rounded-lg"}>Register</Button>
+          </Link>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-      <div className="navbar-end gap-2">
-        {/* theme toggle */}
-        <ThemeToggle></ThemeToggle>
-        {/* authentication links */}
-        <Link href={"/login"}>
-          <Button className={"btn-primary btn-outline rounded-lg"}>
-            Login
-          </Button>
-        </Link>
-        <Link href={"/register"}>
-          <Button className={"btn-primary rounded-lg"}>Register</Button>
-        </Link>
-      </div>
-    </div>
-  );
+    );
+  }
+  else{
+    return <></>
+  }
 };
 
 export default NavBar;
